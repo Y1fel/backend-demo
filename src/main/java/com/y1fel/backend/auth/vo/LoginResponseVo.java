1ff568004ae0452f63f865e8f3bb169d1b75dd1d
Y1fel <rechardifelly@gmail.com>
2026-07-15 11:10:24 +0800

    用户鉴权登录相关

--- src/main/java/com/y1fel/backend/auth/vo/LoginResponseVo.java
+++ src/main/java/com/y1fel/backend/auth/vo/LoginResponseVo.java
@@ -1,5 +1,10 @@
 package com.y1fel.backend.auth.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class LoginResponseVo {
     private String token;
     private String username;
